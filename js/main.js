2190c3203a1715cf82a66f8d8d676d465666812f
ACtheCreator <anthonyjames.clarke@gmail.com>
2022-05-14 21:43:40 +0100

    removal of the locations object

--- js/main.js
+++ js/main.js
@@ -1,39 +1,4 @@
-const locations = [{
-        title: 'City of york',
-        image: 'images\card-1.jpg',
-        description: 'Go See the old Cathedral city. Our guide Mollie will be happy to tell you stories of the old streets on one of out walking tours',
-        showButton: true
-    }, {
-        title: null,
-        image: 'images\card-2.jpg',
-        description: null,
-        showButton: false
-    },
-    {
-        title: null,
-        image: 'images\card-3.jpg',
-        description: null,
-        showButton: false
-    },
-    {
-        title: null,
-        image: 'images\card-4.jpg',
-        description: null,
-        showButton: false
-    },
-    {
-        title: null,
-        image: 'images\card-5.jpg',
-        description: null,
-        showButton: false
-    },
-    {
-        title: 'Cornwall',
-        image: 'images\card-6.jpg',
-        description: 'Go take a dip in the bliss that is the English Sea, and enjoy all Cornwall has to offer',
-        showButton: true
-    }
-]
+
 
 const modal = document.querySelector(".modal");
 const openModal = document.querySelectorAll(".location__details__container__btn");
